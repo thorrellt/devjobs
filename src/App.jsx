645ee42850f0487999c75816ce7d8f797556f28d
ThorrellT <thorrellt@gmail.com>
2022-11-09 08:09:44 -0500

    add button styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,25 @@ function App() {
 
   return (
     <div className="App">
-      <h1>This is an H1 class</h1>
-      <h2>This is an H2 class</h2>
-      <h3>This is an H3 class</h3>
-      <h4>This is an H4 class</h4>
-      <p>This is an p class</p>
+      <div className="light">
+        <h1>This is an H1 class</h1>
+        <h2>This is an H2 class</h2>
+        <h3>This is an H3 class</h3>
+        <h4>This is an H4 class</h4>
+        <h5>This is an H5 class</h5>
+        <p>This is an p class</p>
+        <button className="prim-btn">Button 1</button>
+        <button className="sec-btn-light">Button 2</button>
+      </div>
+      <div className="dark">
+        <h1 className="dark-font">This is an H1 class</h1>
+        <h2 className="dark-font">This is an H2 class</h2>
+        <h3 className="dark-font">This is an H3 class</h3>
+        <h4>This is an H4 class</h4>
+        <h5>This is an H5 class</h5>
+        <p>This is an p class</p>
+        <button className="sec-btn-dark">Button 2</button>
+      </div>
     </div>
   )
 }
