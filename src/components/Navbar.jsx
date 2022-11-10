bb5e65cf05d3a88868f3e052cb910107dcba51a5
ThorrellT <thorrellt@gmail.com>
2022-11-10 14:53:36 -0500

    add switch animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
+import { useState, useContext } from 'react'
+import { DisplayContext } from '../App'
 import '../styles/Navbar.css'
 import bg from '../assets/mobile/bg-pattern-header.svg'
 import icon from '../assets/desktop/logo.svg'
 import sun from '../assets/desktop/icon-sun.svg'
 import moon from '../assets/desktop/icon-moon.svg'
-import { useState, useContext } from 'react'
-import { DisplayContext } from '../App'
 
 const Navbar = (props) => {
   const { darkMode, switchMode } = useContext(DisplayContext)
@@ -13,13 +13,11 @@ const Navbar = (props) => {
     <nav>
       <div className="nav-content flex-container">
         <img src={icon} alt="dev jobs icon" />
+
         <div className="mode-toggle flex-container">
           <img src={sun} className="icon sun" alt="" />
-          <div
-            onClick={switchMode}
-            className={`toggle-switch flex-container ${darkMode ? 'dark' : ''}`}
-          >
-            <div className="selector"></div>
+          <div onClick={switchMode} className="toggle-switch">
+            <div className={`selector ${darkMode ? 'dark' : ''}`}></div>
           </div>
           <img src={moon} className="icon moon" alt="" />
         </div>
