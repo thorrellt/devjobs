a4abffd61982ab0c5a098eaab42509461070a787
ThorrellT <thorrellt@gmail.com>
2022-11-11 09:29:02 -0500

    fix nav svg ratio issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/devjobs/',
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react()],
 })
