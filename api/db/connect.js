040e7c26a3a7ac1be3a5c3da0340dabc2c352424
Thorrell T <thorrellt@gmail.com>
2022-11-22 22:21:05 +0000

    add db connection logic

---
+++ api/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://admin:Ssmith89@devjobs-cluster.elu5wln.mongodb.net/devjobs?retryWrites=true&w=majority'
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(connectionString)
+    .then(()=>{console.log('CONNECTED TO THE DB...')})
+    .catch((err) => console.log(err))
+} 
+
+module.exports = connectDB
