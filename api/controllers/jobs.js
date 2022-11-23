d964c1f02418a93b75c30de95678b55a74e35c9f
ThorrellT <thorrellt@gmail.com>
2022-11-23 10:35:09 -0500

    impliment get, gat all, and create controllers

--- api/controllers/jobs.js
+++ api/controllers/jobs.js
@@ -1,18 +1,36 @@
 const Job = require('../models/Job')
 
-const getAllJobs = (req, res) => {
-  res.send('get all jobs')
+const getAllJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({})
+    res.status(200).json({ jobs })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const getJob = (req, res) => {
-  const { id } = req.params
-  res.send(`get job id:${id}`)
+const getJob = async (req, res) => {
+  try {
+    const { id: jobID } = req.params
+    const job = await Job.findOne({ _id: jobID })
+
+    if (!job) {
+      return res.status(404).jos({ msg: `No take with id : $JobID` })
+    }
+    res.status(200).json({ job })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createJob = async (req, res) => {
-  const job = await Job.create(req.body)
-  res.status(201).json({ job })
-  // res.json(req.body)
+  try {
+    const job = await Job.create(req.body)
+    res.status(201).json({ job })
+    // res.json(req.body)
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const updateJob = async (req, res) => {
