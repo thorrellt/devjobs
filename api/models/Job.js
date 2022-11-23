6d0b0d0485a008c7f9d367f2118d433867d5514a
ThorrellT <thorrellt@gmail.com>
2022-11-23 09:14:57 -0500

    update job schema validation

--- api/models/Job.js
+++ api/models/Job.js
@@ -1,23 +1,59 @@
 const mongoose = require('mongoose')
 
 const JobSchema = new mongoose.Schema({
-  company: String,
-  logo: String,
-  logoBackground: String,
-  position: {
+  company: {
+    type: String,
+    required: [true, 'must include company name'],
+    trim: true,
+  },
+  logo: {
+    type: String,
+    required: [true, 'must include company name'],
+    trim: true,
+  },
+  logoBackground: {
+    type: String,
+    required: [true, 'must include company name'],
+    trim: true,
+  },
+  contract: {
     type: String,
     enum: ['Part Time', 'Full Time', 'Freelance'],
   },
-  postedAt: String,
-  contract: String,
-  location: String,
-  roleDescription: String,
+  postedAt: {
+    type: String,
+    required: [true, 'must include when job was posted'],
+    trim: true,
+  },
+  position: {
+    type: String,
+    required: [true, 'must include position'],
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: [true, 'must include location'],
+    trim: true,
+  },
+  roleDescription: {
+    type: String,
+    required: [true, 'must include role description'],
+    trim: true,
+  },
   requirements: {
-    content: String,
+    content: {
+      type: String,
+      required: [true, 'must include requirements content name'],
+      trim: true,
+    },
     items: [String],
   },
   role: {
-    content: String,
+    content: {
+      type: String,
+      required: [true, 'must include role content'],
+      trim: true,
+    },
     items: [String],
   },
 })
