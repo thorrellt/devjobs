1301e501e126319218ae5d83040bf4ba73386ffe
ThorrellT <thorrellt@gmail.com>
2022-11-25 08:54:45 -0500

    add express async errors & update error handling

--- api/app.js
+++ api/app.js
@@ -1,17 +1,23 @@
+require('dotenv').config()
+require('express-async-errors')
+
 const express = require('express')
 const app = express()
 const jobs = require('./routes/jobs')
 const port = 5000
 const connectDB = require('./db/connect')
-require('dotenv').config()
+
 const notFound = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.json())
 
 // routes
 app.use('/api/v1/jobs', jobs)
+
 app.use(notFound)
+app.use(errorMiddleware)
 
 const start = async () => {
   try {
--- api/controllers/jobs.js
+++ api/controllers/jobs.js
@@ -1,96 +1,70 @@
 const Job = require('../models/Job')
-
+const { createCustomError, CustomAPIError } = require('../errors/custom-error')
 const getAllJobs = async (req, res) => {
-  try {
-    /**
-     * had to format filters to be less strict or
-     * else only exact matches would return
-     */
-    console.log('get all jobs pinged')
-    const filters = {}
-    if ('contract' in req.body) filters.contract = req.body.contract
-    if ('location' in req.body)
-      filters.location = new RegExp(req.body.location, 'i')
-    if ('position' in req.body)
-      filters.position = new RegExp(req.body.position, 'i')
+  /**
+   * SEARCH FILTER SETUP
+   * had to format filters to be less strict or
+   * else only exact matches would return
+   */
+  console.log('get all jobs pinged')
+  const filters = {}
+  if ('contract' in req.body) filters.contract = req.body.contract
+  if ('location' in req.body)
+    filters.location = new RegExp(req.body.location, 'i')
+  if ('position' in req.body)
+    filters.position = new RegExp(req.body.position, 'i')
 
-    const jobs = await Job.find(filters)
-    res.status(200).json({ jobs })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
+  const jobs = await Job.find(filters)
+  res.status(200).json({ jobs })
 }
 
 const getJob = async (req, res) => {
-  try {
-    const { id: jobID } = req.params
-    const job = await Job.findOne({ _id: jobID })
+  const { id: jobID } = req.params
+  const job = await Job.findOne({ _id: jobID })
 
-    if (!job) {
-      return res.status(404).jos({ msg: `No job with id : $JobID` })
-    }
-    res.status(200).json({ job })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  if (!job) {
+    throw new CustomAPIError(`No task with id : ${jobID}`, 404)
   }
+  res.status(200).json({ job })
 }
 
 const createJob = async (req, res) => {
-  try {
-    const job = await Job.create(req.body)
-    res.status(201).json({ job })
-    // res.json(req.body)
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
+  const job = await Job.create(req.body)
+  res.status(201).json({ job })
+  // res.json(req.body)
 }
 
 const deleteJob = async (req, res) => {
-  try {
-    const { id: jobID } = req.params
-    const job = await Job.findOneAndDelete({ _id: jobID, canPatch: true })
+  const { id: jobID } = req.params
+  const job = await Job.findOneAndDelete({ _id: jobID, canPatch: true })
 
-    if (!job) {
-      return res.status(404).json({
-        msg: `No job with id : ${jobID} or this entry cannot be deleted`,
-      })
-    }
-    res.status(200).json({ job })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  if (!job) {
+    throw new CustomAPIError(
+      `No task with id : ${jobID} or this entry cannot be deleted`,
+      404
+    )
   }
+
+  res.status(200).json({ job })
 }
 
 const updateJob = async (req, res) => {
-  try {
-    const { id: jobID } = req.params
+  const { id: jobID } = req.params
 
-    const job = await Job.findOneAndUpdate(
-      { _id: jobID, canPatch: true },
-      req.body,
-      {
-        new: true,
-      }
+  const job = await Job.findOneAndUpdate(
+    { _id: jobID, canPatch: true },
+    req.body,
+    { new: true, runValidators: true }
+  )
+
+  if (!job) {
+    throw new CustomAPIError(
+      `No task with id : ${jobID} or this job cannot be altered`,
+      404
     )
-
-    // const jobs = await Job.updateMany(
-    //   {},
-    //   { canPatch: false },
-    //   {
-    //     new: true,
-    //   }
-    // )
-
-    if (!job) {
-      return res.status(404).json({
-        msg: `No job with id : ${jobID} or this job cannot be altered `,
-      })
-    }
-
-    res.status(200).json({ id: jobID, data: req.body })
-  } catch (error) {
-    res.status(500).json({ msg: error })
   }
+
+  res.status(200).json({ id: jobID, data: req.body })
 }
 
 module.exports = {
