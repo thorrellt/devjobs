49342022996a0088346fc59ca7f5040fce29e6d0
ThorrellT <thorrellt@gmail.com>
2022-12-05 06:49:08 -0500

    add decurity packages

--- server/app.js
+++ server/app.js
@@ -1,17 +1,32 @@
 require('dotenv').config()
 require('express-async-errors')
 
+// Extra Security Packages
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+const cors = require('cors')
+
 const express = require('express')
 const app = express()
 const jobs = require('./routes/jobs')
 const connectDB = require('./db/connect')
-const cors = require('cors')
 
 const notFound = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
 // middleware
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/jobs', jobs)
