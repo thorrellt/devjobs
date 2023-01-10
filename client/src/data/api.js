2d98a3058acaacc4b8e9a3088eee886165809553
ThorrellT <thorrellt@gmail.com>
2023-01-10 08:36:33 -0500

    finish generateJob logic

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -5,12 +5,12 @@ const prodURL = 'https://devjobs-api-08.herokuapp.com/api/v1'
 const currURL = prodURL
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
-    max: 6,
+    max: 5,
     min: 3,
   },
   wordsPerSentence: {
     max: 12,
-    min: 7,
+    min: 6,
   },
 })
 
@@ -78,7 +78,7 @@ export const getJob = async (id) => {
   }
 }
 
-const generateItem = () => {
+const generateItems = () => {
   const numOfItems = Math.floor(Math.random() * 5) + 2
   var items = []
   for (let i = 0; i < numOfItems; i++) {
@@ -87,45 +87,100 @@ const generateItem = () => {
   return items
 }
 
-// var items = generateItem()
-// console.log(items)
+const generateTime = () => {
+  const numOfTime = Math.floor(Math.random() * 6) + 1
+  var randTimeNum = Math.floor(Math.random() * 2)
+  var timeUnit
+  if (randTimeNum == 0) timeUnit = 'd'
+  if (randTimeNum == 1) timeUnit = 'w'
 
-const generateJob = (jobData) => {
-  var job = {
-    company: jobData.company,
+  return `${numOfTime}${timeUnit} ago`
+}
+
+const companyData = {
+  blogr: {
+    name: 'Blogr',
+    logo: 'blogr.svg',
+    logoBackground: 'hsl(12, 79%, 52%)',
+  },
+  creative: {
+    name: 'Creative',
     logo: 'creative.svg',
     logoBackground: 'hsl(295, 55%, 21%)',
+  },
+  coffeeroasters: {
+    name: 'Coffeeroasters',
+    logo: 'coffeeroasters.svg',
+    logoBackground: 'hsl(29, 60%, 87%)',
+  },
+  crowdfund: {
+    name: 'Crowdfund',
+    logo: 'crowdfund.svg',
+    logoBackground: 'hsl(157, 57%, 50%)',
+  },
+  maker: {
+    name: 'Maker',
+    logo: 'maker.svg',
+    logoBackground: 'hsl(218, 58%, 31%)',
+  },
+  mastercraft: {
+    name: 'Mastercraft',
+    logo: 'mastercraft.svg',
+    logoBackground: 'hsl(0, 0%, 12%)',
+  },
+  officeLite: {
+    name: 'Office Lite',
+    logo: 'officeLite.svg',
+    logoBackground: 'hsl(227, 62%, 48%)',
+  },
+  pod: { name: 'Pod', logo: 'pod.svg', logoBackground: 'hsl(216, 46%, 14%)' },
+  pomodoro: {
+    name: 'Pomodoro',
+    logo: 'pomodoro.svg',
+    logoBackground: 'hsl(216, 46%, 14%)',
+  },
+  scoot: {
+    name: 'Scoot',
+    logo: 'scoot.svg',
+    logoBackground: 'hsl(36, 87%, 49%)',
+  },
+  typemaster: {
+    name: 'Typemaster',
+    logo: 'typemaster.svg',
+    logoBackground: 'hsl(22, 89%, 52%)',
+  },
+  vector: {
+    name: 'Vector',
+    logo: 'vector.svg',
+    logoBackground: 'hsl(235, 10%, 23%)',
+  },
+}
+
+const generateJob = (jobData) => {
+  const company = companyData[jobData.company]
+  var job = {
+    company: company.name,
+    logo: company.logo,
+    logoBackground: company.logoBackground,
     position: jobData.position,
-    postedAt: '1d ago',
+    postedAt: generateTime(),
     contract: jobData.contract,
     location: jobData.location,
     requirements: {
       content: lorem.generateParagraphs(1),
-      items: [
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        '5+ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        '3+ years Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      ],
+      items: generateItems(),
     },
     role: {
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet cursus sit amet dictum sit amet justo donec. Tempus egestas sed sed risus pretium quam vulputate.',
-      items: [
-        'Lead, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        'Implement Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        'Oversee Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        'Provide Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do .',
-        'Develop ',
-      ],
+      content: lorem.generateParagraphs(1),
+      items: generateItems(),
     },
-    roleDescription:
-      'Creative is seeking a Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas ultricies.',
+    roleDescription: lorem.generateParagraphs(1),
   }
 
   console.log(job)
 }
 const validJob = {
-  company: 'Rando Inc',
+  company: 'blogr',
   contract: 'Full Time',
   position: 'Full Stack Dev',
   location: 'France',
