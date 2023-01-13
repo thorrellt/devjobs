1388d229e31121df30bb51dbf8754842e9494f5d
ThorrellT <thorrellt@gmail.com>
2023-01-13 08:17:30 -0500

    add activeAuth on server

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const cors = require('cors')
 
 const UserController = require('../controllers/user')
+const checkAuth = require('../middleware/check-auth')
 
 router.post('/signup', UserController.user_signup)
 
